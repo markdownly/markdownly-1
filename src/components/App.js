5b8b84364ea74d489da91413e8756bb2cfc16fc2
Kate Dameron <katerj33@gmail.com>
2019-03-01 10:39:07 -0800

    SaveMarkdown container is noted in App.js until full functionality is available

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Document from '../containers/Document';
-import SaveMarkdown from '../containers/SaveMarkdown';
+// import SaveMarkdown from '../containers/SaveMarkdown';
 
 export default function App() {
   return (
     <>
       <h1>Markdown Editor</h1>
-      <SaveMarkdown />
+      {/* <SaveMarkdown /> */}
       <Document />
     </>
   );
